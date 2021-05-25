62e7b05ff1c832f5f4b2d99ad6849d12979320ca
Adam Bodzak <bodzak.adam@gmail.com>
2021-05-25 12:45:28 +0200

    add navigation menu

--- src/App.js
+++ src/App.js
@@ -1,50 +1,31 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+//View import
 import AboutMeView from './Views/AboutMeView/AboutMeView';
 import SkillsView from './Views/SkillsView/SkillsView';
 import ProjectsView from './Views/ProjectsView/ProjectsView';
 import ContactView from './Views/ContactView/ContactView';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <>
-      <p>Tu będzie moje portfolio</p>
       <BrowserRouter>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <NavLink to='/'>O mnie</NavLink>
-              </li>
-              <li>
-                <NavLink to='/skills'>Umiejętności</NavLink>
-              </li>
-              <li>
-                <NavLink to='/projects'>Projekty</NavLink>
-              </li>
-              <li>
-                <NavLink to='/contact'>Kontakt</NavLink>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route path='/skills' >
-              <SkillsView />
-            </Route>
-            <Route path='/projects' >
-              <ProjectsView />
-            </Route>
-            <Route path='/contact' >
-              <ContactView />
-            </Route>
-            <Route path='/' >
-              <AboutMeView />
-            </Route>
-          </Switch>
-        </div>
+        <Header />
+        <Switch>
+          <Route path='/skills' >
+            <SkillsView />
+          </Route>
+          <Route path='/projects' >
+            <ProjectsView />
+          </Route>
+          <Route path='/contact' >
+            <ContactView />
+          </Route>
+          <Route path='/' >
+            <AboutMeView />
+          </Route>
+        </Switch>
       </BrowserRouter>
-    </>
   );
 }
 
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './Navigation.module.scss';
 
-const Navigation = () => {
+function App() {
+  return (
+    <nav className={styles.Nav}>
+        <ul className={styles.Nav_List}>
+            <li className={styles.Nav__Item}>
+                <NavLink className={styles.Nav__Item__NavLink} to='/'>
+                    O mnie
+                </NavLink>
+            </li>
+            <li className={styles.Nav__Item}>
+                <NavLink className={styles.Nav__Item__NavLink} to='/skills'>
+                    Umiejętności
+                </NavLink>
+            </li>
+            <li className={styles.Nav__Item}>
+                <NavLink className={styles.Nav__Item__NavLink} to='/projects'>
+                    Projekty
+                </NavLink>
+            </li>
+            <li className={styles.Nav__Item}>
+                <NavLink className={styles.Nav__Item__NavLink} to='/contact'>
+                    Kontakt
+                </NavLink>
+            </li>
+        </ul>
+    </nav>
+  );
+}
 
-};
-
-export default Navigation;
+export default App;
