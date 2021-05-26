a824e1a18b56b15368f0b618fe6549ee61ace450
Adam Bodzak <bodzak.adam@gmail.com>
2021-05-26 18:31:21 +0200

    add import projects from GitHub in projects component

---
+++ src/components/Project/Project.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import githubImageWhite from '../../assets/img/GitHubIcon_white.svg';
+import circlesTopImage from '../../assets/img/circles_top.svg';
+import styles from './Project.module.scss';
+
+const Project = ({ name, description, homepage, svn_url }) => {
+    return(
+        <article className={styles.oneProject}>
+            <div className={styles.topBar}>
+                <img className={styles.circlesTopImage} src={circlesTopImage} alt={'Circles'} />
+            </div>
+            <img className={styles.githubIcon} src={githubImageWhite} alt={'GitHub'} />
+            <div className={styles.projInformation}>
+                <h2 className={`${styles.projTitle} ${styles.leftInfo}`}>project:</h2><h2 className={`${styles.projTitle} ${styles.projTitle__bold}`}>{name}</h2>
+                <span className={`${styles.projDescription} ${styles.leftInfo}`}>description:</span><span className={`${styles.projDescription} ${styles.justify}`}>{description}</span>
+                <span className={`${styles.projDemo} ${styles.leftInfo}`}>demo:</span><a href={homepage} className={`${styles.projDemo} ${styles.blue}`}>see here</a>
+                <span className={`${styles.projGithub} ${styles.leftInfo}`}>github:</span><a href={svn_url} className={`${styles.projGithub} ${styles.blue}`}>source code</a>
+            </div>
+        </article>
+    )
+};
+
+export default Project;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import styles from './Projects.module.scss';
-import githubImageWhite from '../../assets/img/GitHubIcon_white.svg';
-import circlesTopImage from '../../assets/img/circles_top.svg';
+import Project from '../Project/Project';
 
-const Projects = () => {
-    return(
-        <section className={styles.section}>
-            <h1 className={styles.headerView}>Moje ostatnie projekty</h1>
-            <p className={styles.paragraf}>Swoją ścieżkę programisty rozpocząłem około roku 2000. Od tego czasu stworzyć kilka projektów. Od marca 2020r. posiadam konto na GitHub-ie i oto lista kilku projektów które chciałem zaprezentować.</p>
-            <div className={styles.allProjects}>
-                <article className={styles.oneProject}>
-                    <div className={styles.topBar}>
-                        <img className={styles.circlesTopImage} src={circlesTopImage} alt={'Circles'} />
-                    </div>
-                    <img className={styles.githubIcon} src={githubImageWhite} alt={'GitHub'} />
-                    <div className={styles.projInformation}>
-                        <h2 className={`${styles.projTitle} ${styles.leftInfo}`}>project:</h2><h2 className={`${styles.projTitle} ${styles.projTitle__bold}`}>Aaaa</h2>
-                        <span className={`${styles.projDescription} ${styles.leftInfo}`}>description:</span><span className={styles.projDescription}>Aaaa sgdregd fhndf hdfghdf j d jghf jgf jty j dfj tyj fj dtj tykj ty juity uj gf j ty k tk kj yk uu k</span>
-                        <span className={`${styles.projDemo} ${styles.leftInfo}`}>demo:</span><span className={`${styles.projDemo} ${styles.blue}`}>Aaaa</span>
-                        <span className={`${styles.projGithub} ${styles.leftInfo}`}>github:</span><span className={`${styles.projGithub} ${styles.blue}`}>Aaaa</span>
-                    </div>
-                </article>
-            </div>
-        </section>
-    )
+import githubImageWhite from '../../assets/img/GitHubIcon_white.svg';
+import circlesTopImage from '../../assets/img/circles_top.svg'
+
+class Projects extends React.Component {
+    state = {
+        projectsList: [],
+    }
+
+    componentDidMount() {
+        fetch("https://api.github.com/users/AdamBodzak/repos")
+        .then((res) => res.json())
+        .then((res) => {
+            const array = res.filter((e) => (e.description !== null) )
+            this.setState({
+                projectsList: array
+            })
+        })
+        .catch((error) => console.log(error));
+    }
+
+    render() {
+        console.log(this.state.projectsList)
+        return(
+            <section className={styles.section}>
+                <h1 className={styles.headerView}>Moje ostatnie projekty</h1>
+                <p className={styles.paragraf}>Swoją ścieżkę programisty rozpocząłem około roku 2000. Od tego czasu stworzyć kilka projektów. Od marca 2020r. posiadam konto na GitHub-ie i oto lista kilku projektów które chciałem zaprezentować.</p>
+                <div className={styles.allProjects}>
+                    {this.state.projectsList.map((e) => (
+                        <Project
+                            key={e.id}
+                            name={e.name}
+                            description={e.description}
+                            homepage={e.homepage}
+                            svn_url={e.svn_url}
+                        />
+                    ))}
+                </div>
+            </section>
+        )
+    }
 };
 
 export default Projects;
