7deea4653e46c7c6dab08177e62aa2f201e7f297
Adam Bodzak <bodzak.adam@gmail.com>
2021-06-14 18:03:14 +0200

    add new tag and styling text

--- src/components/KnowSomething/Skills.js
+++ src/components/KnowSomething/Skills.js
@@ -22,28 +22,51 @@ const Skills = () => {
             </div>
             <ul className={styles.courseList}>
                 <li className={styles.courseItem}>
-                    5 godz. kurs <b> React od podstaw </b> prowadzony przez <b> Adama Romanowskiego </b> na platformie <a target={'_blank'} rel={"noreferrer"} href={'https://eduweb.pl/programowanie-i-www/reactjs/react-od-podstaw'}>eduweb.pl</a>. W ramach kursu powstała aplikacja do zarządzania wiadomościami z twitter-a, artykułami oraz notatkami. Zapraszam do obejrzenia mojego repozytorium na <a target={'_blank'} rel={"noreferrer"} href={'https://github.com/AdamBodzak/AdamRomanowski_kursReactOdPodstaw.git'}>GitHub</a> oraz rezultatów mojej pracy na serwerze <a target={'_blank'} rel={"noreferrer"} href={'https://adam-romanowski-kurs-react-od-podstaw-adambodzak.vercel.app/'}>Vercel</a>. Wersja ostateczna projektu została przeze mnie wzbogacona o dodatkowe funkcjonalności o których można szerzej poczytać w pliku <a target={'_blank'} rel={"noreferrer"} href={'https://github.com/AdamBodzak/AdamRomanowski_kursReactOdPodstaw#readme'}> README.md </a> dołączonym do repozytorium GitHub.
+                    <article className={styles.courseItem__article}>
+                        5 godz. kurs <b> React od podstaw </b> prowadzony przez <b> Adama Romanowskiego </b> na platformie <a target={'_blank'} rel={"noreferrer"} href={'https://eduweb.pl/programowanie-i-www/reactjs/react-od-podstaw'}>eduweb.pl</a>. W ramach kursu powstała aplikacja do zarządzania wiadomościami z twitter-a, artykułami oraz notatkami. Zapraszam do obejrzenia mojego repozytorium na <a target={'_blank'} rel={"noreferrer"} href={'https://github.com/AdamBodzak/AdamRomanowski_kursReactOdPodstaw.git'}>GitHub</a> oraz rezultatów mojej pracy na serwerze <a target={'_blank'} rel={"noreferrer"} href={'https://adam-romanowski-kurs-react-od-podstaw-adambodzak.vercel.app/'}>Vercel</a>. Wersja ostateczna projektu została przeze mnie wzbogacona o dodatkowe funkcjonalności o których można szerzej poczytać w pliku <a target={'_blank'} rel={"noreferrer"} href={'https://github.com/AdamBodzak/AdamRomanowski_kursReactOdPodstaw#readme'}> README.md </a> dołączonym do repozytorium GitHub.
+                    </article>
                 </li>
                 <li className={styles.courseItem}>
-                    7 godz. kurs <b> React w praktyce </b> prowadzony przez <b> Adama Romanowskiego </b> na platformie <a target={'_blank'} rel={"noreferrer"} href={'https://eduweb.pl/programowanie-i-www/reactjs/react-w-praktyce'}>eduweb.pl</a>
+                    <article className={styles.courseItem__article}>
+                        7 godz. kurs <b> React w praktyce </b> prowadzony przez <b> Adama Romanowskiego </b> na platformie <a target={'_blank'} rel={"noreferrer"} href={'https://eduweb.pl/programowanie-i-www/reactjs/react-w-praktyce'}>eduweb.pl</a>
+                    </article>
                 </li>
                 <li className={styles.courseItem}>
-                    12 tygodniowy <b> kurs nauki React </b> prowadzony przez <b> Michała Taszyckiego </b> na stronie internetowej <a target={'_blank'} rel={"noreferrer"} href={'https://kursreacta.pl'}>Kurs React-a</a>. Kurs składa się z dwóch części. Część pierwsza <b> Kurs Reacta - Szybki Start </b> w ramach kursu powstała aplikacja której zadaniem jest odtwarzanie muzyki  dostarczanej z serwera zewnętrznego. Zapraszam do obejrzenia mojego repozytorium na <a target={'_blank'} rel={"noreferrer"} href={'https://github.com/AdamBodzak/CourseReact_QuickStart_MichalTaszycki'}>GitHub</a> oraz rezultatów mojej pracy na serwerze <a target={'_blank'} rel={"noreferrer"} href={'https://course-react-quick-start-michal-taszycki.vercel.app/'}>Vercel</a>. Część dróga <b>Kurs Reacta - Fundamenty i Kurs Reacta - Expert</b> W ramach kursu powstała aplikacja do odliczania interwałów czasowych dla poszczególnych zadań.
+                    <article className={styles.courseItem__article}>
+                        12 tygodniowy <b> kurs nauki React </b> prowadzony przez <b> Michała Taszyckiego </b> na stronie internetowej <a target={'_blank'} rel={"noreferrer"} href={'https://kursreacta.pl'}>Kurs React-a</a>. Kurs składa się z dwóch części:
+                        <p className={styles.courseItem__paragraf}>
+                            - Część pierwsza <b> Kurs Reacta - Szybki Start </b> w ramach kursu powstała aplikacja której zadaniem jest odtwarzanie muzyki  dostarczanej z serwera zewnętrznego. Zapraszam do obejrzenia mojego repozytorium na <a target={'_blank'} rel={"noreferrer"} href={'https://github.com/AdamBodzak/CourseReact_QuickStart_MichalTaszycki'}>GitHub</a> oraz rezultatów mojej pracy na serwerze <a target={'_blank'} rel={"noreferrer"} href={'https://course-react-quick-start-michal-taszycki.vercel.app/'}>Vercel</a>.
+                        </p>
+                        <p className={styles.courseItem__paragraf}>
+                            - Część dróga <b>Kurs Reacta - Fundamenty i Kurs Reacta - Expert</b> W ramach kursu powstała aplikacja do odliczania interwałów czasowych dla poszczególnych zadań.
+                        </p>
+
+                    </article>
                 </li>
                 <li className={styles.courseItem}>
-                    3 Modułowy kurs <b> Opanuj JavaScript </b> prowadzony przez <b> Adam Gospodarczyk, Przemek Smyrdek, Marcin Czarkowski </b> organizowany przez <a target={'_blank'} rel={"noreferrer"} href={'https://przeprogramowani.pl/kurs-javascript'}>Przeprogramowani</a>. W ramach kursu zrealizowałem ponad 60 zadań i 9 projektów które ugruntowały moją wiedzę z zakresu poszczególnych metod JavaScript-u.
+                    <article className={styles.courseItem__article}>
+                        3 Modułowy kurs <b> Opanuj JavaScript </b> prowadzony przez <b> Adam Gospodarczyk, Przemek Smyrdek, Marcin Czarkowski </b> organizowany przez <a target={'_blank'} rel={"noreferrer"} href={'https://przeprogramowani.pl/kurs-javascript'}>Przeprogramowani</a>. W ramach kursu zrealizowałem ponad 60 zadań i 9 projektów które ugruntowały moją wiedzę z zakresu poszczególnych metod JavaScript-u.
+                    </article>
                 </li>
                 <li className={styles.courseItem}>
-                    10 godz. kurs <b> JavaScrypt w Praktyce </b> prowadzony przez <b> Piotr Palarz </b> na platformie <a target={'_blank'} rel={"noreferrer"} href={'https://eduweb.pl/programowanie-i-www/javascript/javascript-w-praktyce'}>eduweb.pl</a>. Stanowi idealne rozszerzenie, uzupełnienie kursu organizowanego przez Przeprogramowanych.
+                    <article className={styles.courseItem__article}>
+                        10 godz. kurs <b> JavaScrypt w Praktyce </b> prowadzony przez <b> Piotr Palarz </b> na platformie <a target={'_blank'} rel={"noreferrer"} href={'https://eduweb.pl/programowanie-i-www/javascript/javascript-w-praktyce'}>eduweb.pl</a>. Stanowi idealne rozszerzenie, uzupełnienie kursu organizowanego przez Przeprogramowanych.
+                    </article>
                 </li>
                 <li className={styles.courseItem}>
-                    13 tygodniowy kurs prowadzony przez <b> Macieja Korsana </b> na stronie internetowej <a target={'_blank'} rel={"noreferrer"} href={'https://cotenfrontend.pl'}>WTF: Co Ten Frontend</a>. W ramach wykonywania masy zadań, nauczyłem się podstaw nowoczesnego frontendu. Dowiedziałem się czym są i jak działają potrzebne w codziennej pracy narzędzia.
+                    <article className={styles.courseItem__article}>
+                        13 tygodniowy kurs prowadzony przez <b> Macieja Korsana </b> na stronie internetowej <a target={'_blank'} rel={"noreferrer"} href={'https://cotenfrontend.pl'}>WTF: Co Ten Frontend</a>. W ramach wykonywania masy zadań, nauczyłem się podstaw nowoczesnego frontendu. Dowiedziałem się czym są i jak działają potrzebne w codziennej pracy narzędzia.
+                    </article>
                 </li>
                 <li className={styles.courseItem}>
-                    7 Modułowy kurs <b> GraphQL Mastery </b> prowadzony przez <b> Michała Taszyckiego </b> na stronie internetowej <a target={'_blank'} rel={"noreferrer"} href={'https://graphqlmastery.pl'}>GraphQL Mastery</a>. W ramach kursu powstała aplikacja back-end używając Apollo Server i Node.js, oraz front-end w React.js, Chakra UI i React Router.
+                    <article className={styles.courseItem__article}>
+                        7 Modułowy kurs <b> GraphQL Mastery </b> prowadzony przez <b> Michała Taszyckiego </b> na stronie internetowej <a target={'_blank'} rel={"noreferrer"} href={'https://graphqlmastery.pl'}>GraphQL Mastery</a>. W ramach kursu powstała aplikacja back-end używając Apollo Server i Node.js, oraz front-end w React.js, Chakra UI i React Router.
+                    </article>
                 </li>
                 <li className={styles.courseItem}>
-                    3,5 godz. kurs <b> Figma projektowanie interfejsów </b> prowadzony przez <b> Grzegorza Rógę </b> na platformie <a target={'_blank'} rel={"noreferrer"} href={'https://eduweb.pl/grafika-i-interfejsy/figma/figma-projektowanie-interfejsow'}>eduweb.pl</a>
+                    <article className={styles.courseItem__article}>
+                        3,5 godz. kurs <b> Figma projektowanie interfejsów </b> prowadzony przez <b> Grzegorza Rógę </b> na platformie <a target={'_blank'} rel={"noreferrer"} href={'https://eduweb.pl/grafika-i-interfejsy/figma/figma-projektowanie-interfejsow'}>eduweb.pl</a>
+                    </article>
                 </li>
             </ul>
             <p className={styles.paragraf}>
