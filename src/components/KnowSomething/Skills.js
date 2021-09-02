069e491056e9e507c3122a900d24b80bd60b5b50
Adam Bodzak <62793146+AdamBodzak@users.noreply.github.com>
2021-09-02 14:06:45 +0200

    change information about course and skills

--- src/components/KnowSomething/Skills.js
+++ src/components/KnowSomething/Skills.js
@@ -26,47 +26,61 @@ const Skills = () => {
               ReactJS
               <ul className={styles.secondUL}>
                 <li className={styles.secondItem}>
-                  React Router i funkcją Switch, NavLink
-                </li>
-                <li className={styles.secondItem}>
-                  props-y i przekazywanie danych
+                  React Router (BrowserRouter, Router, Switch, NavLink)
                 </li>
                 <li className={styles.secondItem}>
                   PropType walidacja zmiennych
+                </li>
+                <li className={styles.secondItem}>
+                  komponenty funkcyjne i class-owe
                 </li>
                 <li className={styles.secondItem}>komponenty class-owe</li>
                 <li className={styles.secondItem}>
                   cykl życia komponentu class-owego
                 </li>
                 <li className={styles.secondItem}>
-                  stan komponentu i jego modyfikacja
+                  props-y i przekazywanie danych
                 </li>
                 <li className={styles.secondItem}>Context API</li>
+                <li className={styles.secondItem}>
+                  stan komponentu i jego modyfikacja
+                </li>
+                <li className={styles.secondItem}>i wiele innych...</li>
               </ul>
             </li>
-            <li className={styles.listItem}>{"JSX (syntactic sugar)"}</li>
-            <li className={styles.listItem}>polyfill</li>
-            <li className={styles.listItem}>Deployment aplikacji na serwer</li>
-            <li className={styles.listItem}>
-              JavaScript ES6+
-              <ul className={styles.secondUL}>
-                <li className={styles.secondItem}>JavaScript</li>
-              </ul>
-            </li>
-            <li className={styles.listItem}>Semantyczny HTML5</li>
             <li className={styles.listItem}>
               CSS i SCSS
               <ul className={styles.secondUL}>
-                <li className={styles.secondItem}>CSS Module</li>
+                <li className={styles.secondItem}>Flexbox</li>
+                <li className={styles.secondItem}>Grid</li>
+                <li className={styles.secondItem}>boxmodel</li>
                 <li className={styles.secondItem}>Animacja CSS i plików SVG</li>
-                <li className={styles.secondItem}>CSS in JS</li>
-                <li className={styles.secondItem}>Flexbox i boxmodel</li>
-                <li className={styles.secondItem}>CSS Grid</li>
+                <li className={styles.secondItem}>i wiele innych...</li>
               </ul>
             </li>
-            <li className={styles.listItem}>obsługa programu Figma</li>
-            <li className={styles.listItem}>obsługa programu Git</li>
-            <li className={styles.listItem}>obsługa programu NPM</li>
+            <li className={styles.listItem}>
+              Obsługa Git (terminal)
+              <ul className={styles.secondUL}>
+                <li className={styles.secondItem}>
+                  tag, branch, checkout, marge, cherry-pick, push, pull, fatch i
+                  wiele innych
+                </li>
+              </ul>
+            </li>
+            <li className={styles.listItem}>{`JSX (syntactic sugar)`}</li>
+            <li className={styles.listItem}>polyfill</li>
+            <li className={styles.listItem}>JavaScript ES6+</li>
+            <li className={styles.listItem}>Styled Components (CSS in JS)</li>
+            <li className={styles.listItem}>CSS Module</li>
+            <li className={styles.listItem}>ESLint - statyczna analiza kodu</li>
+            <li className={styles.listItem}>Prettier - auto formater</li>
+            <li className={styles.listItem}>Storybook V6+</li>
+            <li className={styles.listItem}>Symantyczny HTML5</li>
+            <li className={styles.listItem}>polyfill</li>
+            <li className={styles.listItem}>Obsługa NPM</li>
+            <li className={styles.listItem}>Obsługa Figma</li>
+            <li className={styles.listItem}>Obsługa Visual Studio Code</li>
+            <li className={styles.listItem}>Deployment aplikacji na serwer</li>
             <li className={styles.listItem}>i wiele więcej...</li>
           </ul>
         </article>
@@ -96,12 +110,66 @@ const Skills = () => {
       <p className={styles.paragraf}>
         Programowaniem z pasji zajmuję się od ponad 10 lat, podążając za nowymi
         wyzwaniami, chcąc ciągle poszerzać swoją wiedzę z dziedziny
-        frontend-owej oto następujące szkolenia ki kursy w których uczestniczę:
+        frontend-owej. W ramach kursów wykonałem projekty stron internetowych, a
+        rezultaty mojej pracy umieściłem na serwerze Vercel lub GitHubPage.{" "}
+        <u>
+          Do wielu z projektów starałem się dodać własne dodatkowe
+          funkcjonalności rozszerzając działanie aplikacji
+        </u>
+        , a szerzej o tym można poczytać w plikach readme dodanych do
+        repozytoriów na GitHub. Oto następujące szkolenia i kursy w których
+        uczestniczę:
       </p>
       <ul className={styles.courseList}>
         <li className={styles.courseItem}>
           <article className={styles.courseItem__article}>
-            5 godz. kurs <b> React od podstaw </b> prowadzony przez{" "}
+            <b>14 tygodniowy kurs React-a </b> prowadzony przez{" "}
+            <b> Michała Taszyckiego </b> na stronie internetowej{" "}
+            <a
+              target={"_blank"}
+              rel={"noreferrer"}
+              href={"https://kursreacta.pl"}
+            >
+              Kurs React-a
+            </a>
+            . Kurs składa się z dwóch części:
+            <p className={styles.courseItem__paragraf}>
+              - Część pierwsza <b> Kurs Reacta - Szybki Start </b> powstała
+              aplikacja której zadaniem jest odtwarzanie muzyki dostarczanej z
+              serwera zewnętrznego, wzorowany na elementach Spotify. Zapraszam
+              do obejrzenia mojego repozytorium na{" "}
+              <a
+                target={"_blank"}
+                rel={"noreferrer"}
+                href={
+                  "https://github.com/AdamBodzak/CourseReact_QuickStart_MichalTaszycki"
+                }
+              >
+                GitHub
+              </a>{" "}
+              oraz rezultatów mojej pracy na serwerze{" "}
+              <a
+                target={"_blank"}
+                rel={"noreferrer"}
+                href={
+                  "https://course-react-quick-start-michal-taszycki.vercel.app/"
+                }
+              >
+                Vercel
+              </a>
+              .
+            </p>
+            <p className={styles.courseItem__paragraf}>
+              - Część dróga{" "}
+              <b>Kurs Reacta - Fundamenty i Kurs Reacta - Expert</b> powstała
+              aplikacja do odliczania interwałów czasowych dla poszczególnych
+              zadań. Część druga kursu w trakcie realizacji.
+            </p>
+          </article>
+        </li>
+        <li className={styles.courseItem}>
+          <article className={styles.courseItem__article}>
+            <b>3 tygodniowy kurs React od podstaw</b> prowadzony przez{" "}
             <b> Adama Romanowskiego </b> na platformie{" "}
             <a
               target={"_blank"}
@@ -112,8 +180,8 @@ const Skills = () => {
             >
               eduweb.pl
             </a>
-            . W ramach kursu powstała aplikacja do zarządzania wiadomościami z
-            twitter-a, artykułami oraz notatkami. Zapraszam do obejrzenia mojego
+            . Powstała aplikacja do zarządzania wiadomościami z twitter-a,
+            artykułami oraz notatkami. Zapraszam do obejrzenia mojego
             repozytorium na{" "}
             <a
               target={"_blank"}
@@ -151,7 +219,7 @@ const Skills = () => {
         </li>
         <li className={styles.courseItem}>
           <article className={styles.courseItem__article}>
-            7 godz. kurs <b> React w praktyce </b> prowadzony przez{" "}
+            <b>4 tygodniowy kurs React w praktyce </b> prowadzony przez{" "}
             <b> Adama Romanowskiego </b> na platformie{" "}
             <a
               target={"_blank"}
@@ -166,27 +234,24 @@ const Skills = () => {
         </li>
         <li className={styles.courseItem}>
           <article className={styles.courseItem__article}>
-            12 tygodniowy <b> kurs nauki React </b> prowadzony przez{" "}
+            <b>8 tygodniowy kurs GraphQL Mastery </b> prowadzony przez{" "}
             <b> Michała Taszyckiego </b> na stronie internetowej{" "}
             <a
               target={"_blank"}
               rel={"noreferrer"}
-              href={"https://kursreacta.pl"}
+              href={"https://graphqlmastery.pl"}
             >
-              Kurs React-a
+              GraphQL Mastery
             </a>
             . Kurs składa się z dwóch części:
             <p className={styles.courseItem__paragraf}>
-              - Część pierwsza <b> Kurs Reacta - Szybki Start </b> w ramach
-              kursu powstała aplikacja której zadaniem jest odtwarzanie muzyki
-              dostarczanej z serwera zewnętrznego. Zapraszam do obejrzenia
-              mojego repozytorium na{" "}
+              - Część pierwsza<b> - Szybki Start </b> Powstała aplikacja do
+              generowania inspirujących cytatów. Zapraszam do obejrzenia mojego
+              repozytorium na
               <a
                 target={"_blank"}
                 rel={"noreferrer"}
-                href={
-                  "https://github.com/AdamBodzak/CourseReact_QuickStart_MichalTaszycki"
-                }
+                href={"https://github.com/AdamBodzak/Szkolenie_GraphQL"}
               >
                 GitHub
               </a>{" "}
@@ -194,25 +259,38 @@ const Skills = () => {
               <a
                 target={"_blank"}
                 rel={"noreferrer"}
-                href={
-                  "https://course-react-quick-start-michal-taszycki.vercel.app/"
-                }
+                href={"https://inspiring-quote-iota.vercel.app/"}
               >
                 Vercel
               </a>
               .
             </p>
             <p className={styles.courseItem__paragraf}>
-              - Część dróga{" "}
-              <b>Kurs Reacta - Fundamenty i Kurs Reacta - Expert</b> W ramach
-              kursu powstała aplikacja do odliczania interwałów czasowych dla
-              poszczególnych zadań.
+              - Część dróga <b>Fundamenty</b> Powstanie aplikacja do zażądania
+              książkami w bibliotece. Część druga kursu w trakcie realizacji.
             </p>
+          </article>
+        </li>
+
+        <li className={styles.courseItem}>
+          <article className={styles.courseItem__article}>
+            <b>3 tygodniowy kurs GIT-a </b> {"prowadzony na platformie "}
+            <a
+              target={"_blank"}
+              rel={"noreferrer"}
+              href={"https://kursgita.pl/"}
+            >
+              kurs GIT-a.
+            </a>
+            {` Nauczyłem się pracy z lokalnym jak i zdalnym
+            repozytorium Git-a, oraz zdobyłem praktyczną wiedzę i umiejętność
+            zastosowania takich komend jak tag, branch, checkout, marge,
+            cherry-pick, push, pull, fatch i wiele innych`}
           </article>
         </li>
         <li className={styles.courseItem}>
           <article className={styles.courseItem__article}>
-            3 Modułowy kurs <b> Opanuj JavaScript </b> prowadzony przez{" "}
+            <b>6 tygodniowy kurs Opanuj JavaScript </b> prowadzony przez{" "}
             <b> Adam Gospodarczyk, Przemek Smyrdek, Marcin Czarkowski </b>{" "}
             organizowany przez{" "}
             <a
@@ -222,13 +300,13 @@ const Skills = () => {
             >
               Przeprogramowani
             </a>
-            . W ramach kursu zrealizowałem ponad 60 zadań i 9 projektów które
-            ugruntowały moją wiedzę z zakresu poszczególnych metod JavaScript-u.
+            . Zrealizowałem ponad 60 zadań i 9 projektów które ugruntowały moją
+            wiedzę z zakresu poszczególnych metod JavaScript-u.
           </article>
         </li>
         <li className={styles.courseItem}>
           <article className={styles.courseItem__article}>
-            10 godz. kurs <b> JavaScrypt w Praktyce </b> prowadzony przez{" "}
+            <b>4 tygodniowy kurs JavaScrypt w Praktyce </b> prowadzony przez{" "}
             <b> Piotr Palarz </b> na platformie{" "}
             <a
               target={"_blank"}
@@ -245,7 +323,7 @@ const Skills = () => {
         </li>
         <li className={styles.courseItem}>
           <article className={styles.courseItem__article}>
-            13 tygodniowy kurs prowadzony przez <b> Macieja Korsana </b> na
+            <b>13 tygodniowy kurs prowadzony przez Macieja Korsana </b> na
             stronie internetowej{" "}
             <a
               target={"_blank"}
@@ -254,30 +332,15 @@ const Skills = () => {
             >
               WTF: Co Ten Frontend
             </a>
-            . W ramach wykonywania masy zadań, nauczyłem się podstaw
-            nowoczesnego frontendu. Dowiedziałem się czym są i jak działają
-            potrzebne w codziennej pracy narzędzia.
+            . Nauczyłem się podstaw nowoczesnego frontendu. Dowiedziałem się
+            czym są i jak działają potrzebne w codziennej pracy narzędzia.
           </article>
         </li>
+
         <li className={styles.courseItem}>
           <article className={styles.courseItem__article}>
-            7 Modułowy kurs <b> GraphQL Mastery </b> prowadzony przez{" "}
-            <b> Michała Taszyckiego </b> na stronie internetowej{" "}
-            <a
-              target={"_blank"}
-              rel={"noreferrer"}
-              href={"https://graphqlmastery.pl"}
-            >
-              GraphQL Mastery
-            </a>
-            . W ramach kursu powstała aplikacja back-end używając Apollo Server
-            i Node.js, oraz front-end w React.js, Chakra UI i React Router.
-          </article>
-        </li>
-        <li className={styles.courseItem}>
-          <article className={styles.courseItem__article}>
-            3,5 godz. kurs <b> Figma projektowanie interfejsów </b> prowadzony
-            przez <b> Grzegorza Rógę </b> na platformie{" "}
+            <b>1 tygodniowy kurs Figma projektowanie interfejsów prowadzony</b>{" "}
+            prowadzony przez <b> Grzegorza Rógę </b> na platformie{" "}
             <a
               target={"_blank"}
               rel={"noreferrer"}
