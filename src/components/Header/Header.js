2b1f2153b984f42d9690263dcbe2227c87345ac5
Adam Bodzak <62793146+AdamBodzak@users.noreply.github.com>
2021-09-20 17:57:20 +0200

    fix bug with mobile menu. menu hiden when scroll down page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,46 +1,35 @@
-import React from 'react';
-import Navigation from '../Navigation/Navigation';
-import avatar from '../../assets/img/avatar_300.gif';
-import styles from './Header.module.scss';
-
+import React from "react";
+import Navigation from "../Navigation/Navigation";
+import avatar from "../../assets/img/avatar_300.gif";
+import styles from "./Header.module.scss";
 
 class Header extends React.Component {
-    state = {
-        showMobileMenu: false,
-    }
+  state = {
+    showMobileMenu: false,
+  };
 
-    handleShowMobileMenu = () => {
-        this.setState( prevState => {
-            console.log(prevState.showMobileMenu)
-            return {showMobileMenu: prevState.showMobileMenu ? false : true}
-        })
-    }
-    render() {
-        const {showMobileMenu} = this.state
-        return(
-            <header className={styles.header}>
-                    <img className={styles.avatar} src={avatar} alt={'Avatar'}/>
-                    <div className={styles.wrapper}>
-                        <div className={styles.welcome}>
-                            <p className={styles.speech}>
-                                Miło że jesteś na mojej stronie
-                            </p>
-                            <button onClick={this.handleShowMobileMenu} className={styles.hamburgerMenu__Button}>
-                                <div className={styles.hamburgerMenu__BG} >
-                                    <div className={`${styles.hamburgerMenu__Icon} + ${styles[showMobileMenu ? 'classShowMobileMenu' : null]}`}>
-                                    </div>
-                                </div>
-                            </button>
-                        </div>
-                        <Navigation
-                            fnShowMenu={this.handleShowMobileMenu} 
-                            showMobileMenu={showMobileMenu} 
-                        />
-                    </div>
-            </header>
-        )
-    }
-
+  handleShowMobileMenu = () => {
+    this.setState((prevState) => {
+      return { showMobileMenu: prevState.showMobileMenu ? false : true };
+    });
+  };
+  render() {
+    const { showMobileMenu } = this.state;
+    return (
+      <header className={styles.header}>
+        <img className={styles.avatar} src={avatar} alt={"Avatar"} />
+        <div className={styles.wrapper}>
+          <div className={styles.welcome}>
+            <p className={styles.speech}>Miło że jesteś na mojej stronie</p>
+          </div>
+          <Navigation
+            fnShowMenu={this.handleShowMobileMenu}
+            showMobileMenu={showMobileMenu}
+          />
+        </div>
+      </header>
+    );
+  }
 }
 
 export default Header;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,53 +5,64 @@ import logoAllImage from "../../assets/img/logo.svg";
 
 function App({ showMobileMenu, fnShowMenu }) {
   return (
-    <nav
-      className={`${styles.Nav} + ${
-        styles[showMobileMenu ? "classShowMobileMenu" : null]
-      }`}
-    >
-      <img className={styles.logoImage} src={logoAllImage} alt="logo" />
+    <>
+      <button onClick={fnShowMenu} className={styles.hamburgerMenu__Button}>
+        <div className={styles.hamburgerMenu__BG}>
+          <div
+            className={`${styles.hamburgerMenu__Icon} + ${
+              styles[showMobileMenu ? "classShowMobileMenu" : null]
+            }`}
+          ></div>
+        </div>
+      </button>
+      <nav
+        className={`${styles.Nav} + ${
+          styles[showMobileMenu ? "classShowMobileMenu" : null]
+        }`}
+      >
+        <img className={styles.logoImage} src={logoAllImage} alt="logo" />
 
-      <ul className={styles.Nav_List}>
-        <li onClick={fnShowMenu} className={styles.Nav__Item}>
-          <NavLink
-            exact
-            activeClassName={styles.active}
-            className={styles.Nav__Item__NavLink}
-            to="/"
-          >
-            Umiejętności
-          </NavLink>
-        </li>
-        <li onClick={fnShowMenu} className={styles.Nav__Item}>
-          <NavLink
-            activeClassName={styles.active}
-            className={styles.Nav__Item__NavLink}
-            to="/projects"
-          >
-            Projekty
-          </NavLink>
-        </li>
-        <li onClick={fnShowMenu} className={styles.Nav__Item}>
-          <NavLink
-            activeClassName={styles.active}
-            className={styles.Nav__Item__NavLink}
-            to="/contact"
-          >
-            Kontakt
-          </NavLink>
-        </li>
-        <li onClick={fnShowMenu} className={styles.Nav__Item}>
-          <NavLink
-            activeClassName={styles.active}
-            className={styles.Nav__Item__NavLink}
-            to="/about_me"
-          >
-            O mnie
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+        <ul className={styles.Nav_List}>
+          <li onClick={fnShowMenu} className={styles.Nav__Item}>
+            <NavLink
+              exact
+              activeClassName={styles.active}
+              className={styles.Nav__Item__NavLink}
+              to="/"
+            >
+              Umiejętności
+            </NavLink>
+          </li>
+          <li onClick={fnShowMenu} className={styles.Nav__Item}>
+            <NavLink
+              activeClassName={styles.active}
+              className={styles.Nav__Item__NavLink}
+              to="/projects"
+            >
+              Projekty
+            </NavLink>
+          </li>
+          <li onClick={fnShowMenu} className={styles.Nav__Item}>
+            <NavLink
+              activeClassName={styles.active}
+              className={styles.Nav__Item__NavLink}
+              to="/contact"
+            >
+              Kontakt
+            </NavLink>
+          </li>
+          <li onClick={fnShowMenu} className={styles.Nav__Item}>
+            <NavLink
+              activeClassName={styles.active}
+              className={styles.Nav__Item__NavLink}
+              to="/about_me"
+            >
+              O mnie
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
 
