569b4c88d89f926230b277485e2bd97479d2662b
Adam Bodzak <62793146+AdamBodzak@users.noreply.github.com>
2021-09-21 16:21:49 +0200

    add in header jokes from api Jokes Hallo Roman

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,33 @@ import styles from "./Header.module.scss";
 
 class Header extends React.Component {
   state = {
+    jokes: [],
+    randomNumber: 0,
     showMobileMenu: false,
   };
+
+  componentDidMount() {
+    this.getJokes();
+  }
+
+  randomNumber(jokes) {
+    this.setState({
+      randomNumber: Math.floor(Math.random() * jokes.length),
+    });
+  }
+
+  getJokes() {
+    // connecting to API to retrieve jokes
+    fetch("https://api.jsonbin.io/b/5ff70d0b09f7c73f1b6ec2b4")
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          jokes: res.jokes,
+        });
+        this.randomNumber(res.jokes);
+      })
+      .catch((error) => console.log(error));
+  }
 
   handleShowMobileMenu = () => {
     this.setState((prevState) => {
@@ -14,13 +39,22 @@ class Header extends React.Component {
     });
   };
   render() {
-    const { showMobileMenu } = this.state;
+    const { showMobileMenu, jokes, randomNumber } = this.state;
+    const question = jokes[randomNumber]?.question;
+    const answer = jokes[randomNumber]?.answer;
+
     return (
       <header className={styles.header}>
         <img className={styles.avatar} src={avatar} alt={"Avatar"} />
         <div className={styles.wrapper}>
           <div className={styles.welcome}>
-            <p className={styles.speech}>Miło że jesteś na mojej stronie</p>
+            <div className={styles.speech}>
+              <p className={styles.greeting}>
+                Miło że jesteś na mojej stronie zacznijmy od suchara Romana
+              </p>
+              <p className={styles.joke}>{question}</p>
+              <p className={styles.joke}>{answer}</p>
+            </div>
           </div>
           <Navigation
             fnShowMenu={this.handleShowMobileMenu}
